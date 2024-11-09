f28a664e6f7a5d27a2cb41801ee1392b75d6610a
victor <thysvic@gmail.com>
2024-11-09 19:38:41 +0100

    feat(login): add HTML layout and PHP logic for login page

---
+++ login.php
@@ -0,0 +1,70 @@
+<?php
+
+    function canLogin($username, $password){
+
+        if($username === "victor@shop.be" && $password ==="12345isnotsecure"){
+
+            return true;
+        }
+        else {
+            
+            return false;
+        }
+    }
+
+    if(!empty($_POST)){
+
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        if (canLogin($username, $password)){
+
+            //LOGIN
+            session_start();
+            $_SESSION["username"] = $username;
+            header("Location: index.php");
+        }
+        else {
+
+            //NO LOGIN
+            $error = true;
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+
+    <div class="login_card">
+        
+        <h1>webshop<span class="accent_color">.</span></h1>
+        <form action="" method="post" class="login_form">
+            
+            <h2><i>Login</i></h2>
+
+            <?php if(isset($error)): ?>
+            <p class="error_text">The password or username was incorrect. Please try again</p>
+            <?php endif; ?>
+
+            <label for="username">Username</label>
+            <input type="text" id="username" name="username">
+
+            <label for="username">Password</label>
+            <input type="password" id="password" name="password">
+
+            <input type="submit" value="log in" class="btn">
+            <p>No account? sign up <a href="signup.php">here</a>.</p>
+        </form>
+    </div>
+</body>
+</html>
