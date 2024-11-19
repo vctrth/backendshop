14485432acbc155ab4a1766e99e3cf959c07bdd1
victor <thysvic@gmail.com>
2024-11-19 17:28:47 +0100

    feat(database): add settings to db singleton

--- classes/Db.php
+++ classes/Db.php
@@ -6,9 +6,12 @@
 
         public static function getConnection(){
 
+            include_once(__DIR__.'/../settings/settings.php');
+
             if(self::$conn === null){
 
-                self::$conn = new PDO("mysql:host=127.0.0.1;port=8889;dbname=backendshop", "root", "root");
+                self::$conn = new PDO("mysql:host=" . SETTINGS["db"]["host"] . ";port=" . SETTINGS["db"]["port"] . ";dbname=" . SETTINGS["db"]["dbname"] , SETTINGS["db"]["user"] , SETTINGS["db"]["password"]);
+                // $conn = new PDO("mysql:host=127.0.0.1;port=8889;dbname=backendshop", "root", "root");
                 return self::$conn;
             }
             else {
---
+++ settings/settings.php
@@ -0,0 +1,13 @@
+<?php
+
+const  SETTINGS = [
+
+    "db"=> [
+
+        "dbname" => "backendshop",
+        "user" => "root",
+        "password" => "root",
+        "host" => "localhost",
+        "port" => 8889
+    ]
+];
