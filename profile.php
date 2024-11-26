021b52708ff2109b0470c2721679fd9300247828
victor <thysvic@gmail.com>
2024-11-26 10:41:01 +0100

    feat(profile): add profile page

--- profile.php
+++ profile.php
@@ -24,9 +24,27 @@ else {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>profile settings</title>
+
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+
+    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <nav class="top_nav">
+
+        <div class="left_content">
+            <h3><a href="index.php">webglørpp<span class="accent_color">.</span></a></h3>
+        </div>
+
+        <div class="right_content">
+            <?php if($role === 1): ?><a href="addproduct.php">add product</a><?php endif; ?>
+            <a href="logout.php">logout</a>
+        </div>
+    </nav>
     
+
+    <h2><i>Welcome, <?php echo htmlspecialchars($user ->getUsername()) ?></i></h2>
 </body>
 </html>
