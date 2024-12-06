4e80e2b81a1a2a32ab04b4ceeb73b6fb809e7deb
victor <thysvic@gmail.com>
2024-12-06 13:14:24 +0100

    fix(db): fix getting .env keys in azure

--- classes/Db.php
+++ classes/Db.php
@@ -16,11 +16,16 @@ class Db {
                 PDO::MYSQL_ATTR_SSL_CA => $pathToSSL
             ];
 
-            $host = $_ENV['DB_HOST'];
-            $port = $_ENV['DB_PORT'];
-            $dbname = $_ENV['DB_NAME'];
-            $user = $_ENV['DB_USER'];
-            $password = $_ENV['DB_PASSWORD'];
+            $host = getenv('DB_HOST');
+            $port = getenv('DB_PORT');
+            $dbname = getenv('DB_NAME');
+            $user = getenv('DB_USER');
+            $password = getenv('DB_PASSWORD');
+            // $host = $_ENV['DB_HOST'];
+            // $port = $_ENV['DB_PORT'];
+            // $dbname = $_ENV['DB_NAME'];
+            // $user = $_ENV['DB_USER'];
+            // $password = $_ENV['DB_PASSWORD'];
 
             self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password, $options);
             // self::$conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $password);
